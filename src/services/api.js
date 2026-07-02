31fc6cbb6840aabb44c5a34d94eb63c462aae219
GAURAV KUMAR <153047679+Gaurav1764@users.noreply.github.com>
2026-07-02 13:25:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://edusphere-backend-xw5j.onrender.com/api",
+  baseURL: "https://login-backend-l90h.onrender.com/api",
 });
 
 export default API;
